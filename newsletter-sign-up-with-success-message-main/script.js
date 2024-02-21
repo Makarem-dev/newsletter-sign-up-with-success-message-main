c0cd53473e7098e4a342c794bd58492283937b1d
MohammadAli82 <140317609+MohammadAli82@users.noreply.github.com>
2024-02-21 22:10:10 +0330

    Initial Commit

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,53 +1,47 @@
 "use strict";
 
-// let result = input.includes("@gmail.com");
-
 const input = document.getElementById("EmailTxt");
 const button = document.getElementById("login");
 const alertEL = document.querySelector(".Email");
 
 input.addEventListener("input", () => {
-    validateForm(input.value);
+  validateForm(input.value);
 });
 
 button.addEventListener("click", () => {
-    const isValid = validateForm(input.value);
+  const isValid = validateForm(input.value);
 
-    if (isValid) {
-        window.location.href = `message.html?email=${input.value}&test=2&x=33`;
-        // window.open("message.html");
-    }
+  if (isValid) {
+    window.location.href = `message.html?email=${input.value}&test=2&x=33`;
+    // window.open("message.html");
+  }
 });
 
 const validateForm = function (inputValue) {
-    let errorMessages = [];
+  let errorMessages = [];
 
-    if (inputValue === "") {
-        errorMessages.push("Email is required");
-    }
+  if (inputValue === "") {
+    errorMessages.push("Email is required");
+  }
 
-    if (!inputValue.includes("@gmail.com")) {
-        errorMessages.push("Email is invalid");
-    }
+  if (!inputValue.includes("@gmail.com")) {
+    errorMessages.push("Email is invalid");
+  }
 
-    if (inputValue.includes(".ir")) {
-        errorMessages.push("Iran is not valid");
-    }
+  if (inputValue.includes(".ir")) {
+    errorMessages.push("Iran is not valid");
+  }
 
-    if (errorMessages.length > 0) {
-        alertEL.innerText = errorMessages.join(", ");
-        button.style.filter = "grayscale(1)";
-        button.disabled = true;
+  if (errorMessages.length > 0) {
+    alertEL.innerText = errorMessages.join(", ");
+    button.style.filter = "grayscale(1)";
+    button.disabled = true;
 
-        return false;
-    } else {
-        alertEL.innerText = "Email address";
-        button.style.filter = "grayscale(0)";
-        button.disabled = false;
-        return true;
-    }
+    return false;
+  } else {
+    alertEL.innerText = "Email address";
+    button.style.filter = "grayscale(0)";
+    button.disabled = false;
+    return true;
+  }
 };
-
-
-
-
