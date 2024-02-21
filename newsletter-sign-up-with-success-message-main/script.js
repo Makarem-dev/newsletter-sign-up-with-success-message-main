f842ced54826bb153ca134305d6fd2adbe9d0202
MohammadAli82 <140317609+MohammadAli82@users.noreply.github.com>
2024-02-21 16:31:26 +0330

    Initial Commit

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,5 +1,34 @@
-'use strict';
+"use strict";
 
-// const input = document.getElementById("emailtxt").value;
+// let result = input.includes("@gmail.com");
 
+const input = document.getElementById("EmailTxt");
+const button = document.getElementById("login");
+const alertEL = document.querySelector(".Email");
+button.addEventListener("click", (e) => {
+  let messages = [];
+  if (input.value === "" || input.value == null) {
+    messages.push("Email is required");
+  }
+  if (messages.length > 0) {
+    e.preventDefault(input);
+    alertEL.innerText = messages.join(", ");
+  }
+});
 
+// function subBtn() {
+//   if (input === "") {
+//     alert.textContent = "you did not write email";
+//   }
+//   else if(input !== "") {
+//     alert.textContent = "Email address";
+//   }
+// }
+
+// window.open("message.html")
+//   if (result !== true) {
+//     alert.textContent = "wrong email";
+//     console.log(result);
+//   } else if ((result == true)) {
+//     console.log(result);
+//   }
